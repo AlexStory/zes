8396faf15815bb67091ec4a2435ef886463db1f0
alex story <astory11@yahoo.com>
2025-02-20 08:17:06 -0600

    implemented a bunch of cpu instructions

--- cpu.go
+++ cpu.go
@@ -14,6 +14,9 @@ type Cpu struct {
 
 type StatusFlag uint8
 
+const STACK_START = 0xFD
+const STACK = 0x100
+
 const (
 	CARRY StatusFlag = 1 << iota
 	ZERO
@@ -102,6 +105,10 @@ func (c *Cpu) Run() {
 		programCounterState := c.PC
 
 		switch code {
+		case 0x69, 0x65, 0x75, 0x6D, 0x7D, 0x79, 0x61, 0x71:
+			c.ADC(opcode.Mode)
+		case 0x29, 0x25, 0x35, 0x2D, 0x3D, 0x39, 0x21, 0x31:
+			c.AND(opcode.Mode)
 		case 0xA9, 0xA5, 0xB5, 0xAD, 0xBD, 0xB9, 0xA1, 0xB1:
 			c.LDA(opcode.Mode)
 		case 0xA2, 0xA6, 0xB6, 0xAE, 0xBE:
@@ -116,6 +123,36 @@ func (c *Cpu) Run() {
 			c.STY(opcode.Mode)
 		case 0xE6, 0xF6, 0xEE, 0xFE:
 			c.INC(opcode.Mode)
+		case 0xC6, 0xD6, 0xCE, 0xDE:
+			c.DEC()
+		case 0x0A:
+			c.ASLAccumulator()
+		case 0x06, 0x16, 0x0E, 0x1E:
+			c.ASL(opcode.Mode)
+		// JMP ABSOLUTe
+		case 0x4C:
+			addr := c.read16(c.PC)
+			c.PC = addr
+		// JMP INDIRECT
+		case 0x6C:
+			addr := c.read16(c.PC)
+			var indirect_ref uint16
+			if addr&0x00FF == 0x00FF {
+				lo := c.read(addr)
+				hi := c.read(addr & 0xFF00)
+				indirect_ref = (uint16(hi) << 8) | uint16(lo)
+			} else {
+				indirect_ref = c.read16(addr)
+			}
+			c.PC = indirect_ref
+		case 0x48:
+			c.PHA()
+		case 0x68:
+			c.PLA()
+		case 0x08:
+			c.PHP()
+		case 0x28:
+			c.PLP()
 		case 0xAA:
 			c.TAX()
 		case 0xA8:
@@ -124,9 +161,12 @@ func (c *Cpu) Run() {
 			c.INX()
 		case 0xC8:
 			c.INY()
+		case 0xCA:
+			c.DEX()
+		case 0x88:
+			c.DEY()
 		case 0xEA:
-			// NOP
-			continue
+			c.NOP()
 		case 0x00:
 			return
 		default:
@@ -171,7 +211,36 @@ func (c *Cpu) Reset() {
 	c.Y = 0
 	c.Status = 0
 
+	c.SP = STACK_START
 	c.PC = c.read16(0xFFFC)
+}
+
+func (c *Cpu) ASLAccumulator() {
+	value := c.A << 1
+	c.setZeroAndNegativeFlags(value)
+	if c.A&0x80 > 0 {
+		c.setFlag(CARRY, true)
+	} else {
+		c.setFlag(CARRY, false)
+	}
+	c.A = value
+}
+
+func (c *Cpu) addToA(value byte) {
+	result := c.A + value
+
+	if (c.A^result)&(value^result)&0x80 > 0 {
+		c.setFlag(OVERFLOW, true)
+	} else {
+		c.setFlag(OVERFLOW, false)
+	}
+
+	if result < c.A {
+		c.setFlag(CARRY, true)
+	} else {
+		c.setFlag(CARRY, false)
+	}
+	c.A = result
 }
 
 // Read & Write
@@ -198,6 +267,37 @@ func (c *Cpu) write(addr uint16, data byte) {
 }
 
 // CPU Instructions
+func (c *Cpu) ADC(mode AddressingMode) {
+	addr := c.getOperandAddress(mode)
+	value := c.read(addr)
+	c.addToA(value)
+
+	c.setZeroAndNegativeFlags(c.A)
+}
+
+func (c *Cpu) AND(mode AddressingMode) {
+	addr := c.getOperandAddress(mode)
+	value := c.read(addr)
+	result := c.A & value
+	c.setZeroAndNegativeFlags(result)
+	c.A = result
+}
+
+func (c *Cpu) ASL(mode AddressingMode) {
+	addr := c.getOperandAddress(mode)
+	value := c.read(addr)
+	result := value << 1
+
+	if value&0x80 > 0 {
+		c.setFlag(CARRY, true)
+	} else {
+		c.setFlag(CARRY, false)
+	}
+
+	c.setZeroAndNegativeFlags(result)
+	c.write(addr, result)
+}
+
 func (c *Cpu) LDA(mode AddressingMode) {
 	addr := c.getOperandAddress(mode)
 	value := c.read(addr)
@@ -234,6 +334,27 @@ func (c *Cpu) STY(mode AddressingMode) {
 	c.write(addr, c.Y)
 }
 
+func (c *Cpu) PHA() {
+	c.write(STACK+uint16(c.SP), c.A)
+	c.SP -= 1
+}
+
+func (c *Cpu) PLA() {
+	c.SP += 1
+	c.A = c.read(STACK + uint16(c.SP))
+	c.setZeroAndNegativeFlags(c.A)
+}
+
+func (c *Cpu) PHP() {
+	c.write(STACK+uint16(c.SP), c.Status)
+	c.SP -= 1
+}
+
+func (c *Cpu) PLP() {
+	c.SP += 1
+	c.Status = c.read(STACK + uint16(c.SP))
+}
+
 func (c *Cpu) TAX() {
 	c.X = c.A
 	c.setZeroAndNegativeFlags(c.X)
@@ -242,6 +363,27 @@ func (c *Cpu) TAX() {
 func (c *Cpu) TAY() {
 	c.Y = c.A
 	c.setZeroAndNegativeFlags(c.Y)
+}
+
+func (c *Cpu) DEC() {
+	addr := c.getOperandAddress(ZEROPAGE)
+	value := c.read(addr) - 1
+	c.write(addr, value)
+	c.setZeroAndNegativeFlags(value)
+}
+
+func (c *Cpu) DEX() {
+	c.X -= 1
+	c.setZeroAndNegativeFlags(c.X)
+}
+
+func (c *Cpu) DEY() {
+	c.Y -= 1
+	c.setZeroAndNegativeFlags(c.Y)
+}
+
+func (c *Cpu) NOP() {
+	// No operation
 }
 
 func (c *Cpu) INC(mode AddressingMode) {
--- cpu_test.go
+++ cpu_test.go
@@ -471,6 +471,472 @@ func Test0xFEINCAbsoluteX(t *testing.T) {
 	})
 }
 
+func Test0xCADEX(t *testing.T) {
+	t.Run("decrement x", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA2, 0x05, 0xCA, 0x00})
+
+		testX(t, cpu, 0x04)
+	})
+}
+
+func Test0x88DEY(t *testing.T) {
+	t.Run("decrement y", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA0, 0x05, 0x88, 0x00})
+
+		testY(t, cpu, 0x04)
+	})
+}
+
+func Test0xC6DECZeroPage(t *testing.T) {
+	t.Run("decrement memory", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0x55)
+		cpu.LoadAndRun([]byte{0xC6, 0x05, 0x00})
+
+		testMem(t, cpu, 0x05, 0x54)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0xEANOP(t *testing.T) {
+	t.Run("do nothing", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xEA, 0x00})
+
+		testA(t, cpu, 0x00)
+		testX(t, cpu, 0x00)
+		testY(t, cpu, 0x00)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, OVERFLOW, false)
+	})
+
+	t.Run("NOP doesn't break program", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xEA, 0xA9, 0x05, 0x00})
+
+		testA(t, cpu, 0x05)
+	})
+}
+
+func Test0x48PHA(t *testing.T) {
+	t.Run("push a to stack", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0x05, 0x48, 0x00})
+
+		testStack(t, cpu, 0x05)
+		testStackAddress(t, cpu, 0xFC)
+	})
+}
+
+func Test0x68PLA(t *testing.T) {
+	t.Run("pop a from stack", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0x05, 0x48, 0xA9, 0x00, 0x68, 0x00})
+
+		testA(t, cpu, 0x05)
+	})
+}
+
+func Test0x08PHP(t *testing.T) {
+	t.Run("push status to stack", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0x00, 0x08, 0x00})
+
+		testStack(t, cpu, 0x02)
+		testStackAddress(t, cpu, 0xFC)
+	})
+}
+
+func Test0x28PLP(t *testing.T) {
+	t.Run("pop status from stack", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0b10101010, 0x48, 0x28})
+
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, true)
+		testFlag(t, cpu, INTERRUPT, false)
+		testFlag(t, cpu, DECIMAL, true)
+		testFlag(t, cpu, BREAK, false)
+		testFlag(t, cpu, UNUSED, true)
+		testFlag(t, cpu, OVERFLOW, false)
+		testFlag(t, cpu, NEGATIVE, true)
+	})
+}
+
+func Test0x69ADCImmediate(t *testing.T) {
+	t.Run("add with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0x05, 0x69, 0x02, 0x00})
+
+		testA(t, cpu, 0x07)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+
+	t.Run("add with carry carry bit", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0xFF, 0x69, 0x01, 0x00})
+
+		testA(t, cpu, 0x00)
+		testFlag(t, cpu, CARRY, true)
+		testFlag(t, cpu, ZERO, true)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+
+	t.Run("add with signed overflow", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0x50, 0x69, 0x50, 0x00})
+
+		testA(t, cpu, 0xa0)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, true)
+		testFlag(t, cpu, OVERFLOW, true)
+	})
+}
+
+func Test065ADCZeroPage(t *testing.T) {
+	t.Run("add with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0x02)
+		cpu.LoadAndRun([]byte{0xA9, 0x05, 0x65, 0x05, 0x00})
+
+		testA(t, cpu, 0x07)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x75ADCZeroPageX(t *testing.T) {
+	t.Run("add with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0x02)
+		cpu.LoadAndRun([]byte{0xA2, 0x01, 0xA9, 0x05, 0x75, 0x04, 0x00})
+
+		testA(t, cpu, 0x07)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x6DADCAbsolute(t *testing.T) {
+	t.Run("add with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0200, 0x02)
+		cpu.LoadAndRun([]byte{0xA9, 0x05, 0x6D, 0x00, 0x02, 0x00})
+
+		testA(t, cpu, 0x07)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x7DADCAbsoluteX(t *testing.T) {
+	t.Run("add with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0202, 0x02)
+		cpu.LoadAndRun([]byte{0xA2, 0x02, 0xA9, 0x05, 0x7D, 0x00, 0x02, 0x00})
+
+		testA(t, cpu, 0x07)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x79ADCAbsoluteY(t *testing.T) {
+	t.Run("add with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0202, 0x02)
+		cpu.LoadAndRun([]byte{0xA0, 0x02, 0xA9, 0x05, 0x79, 0x00, 0x02, 0x00})
+
+		testA(t, cpu, 0x07)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x61ADCIndirectX(t *testing.T) {
+	t.Run("add with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0x34)
+		cpu.write(0x06, 0x12)
+		cpu.write(0x1234, 0x02)
+
+		cpu.LoadAndRun([]byte{0xA2, 0x03, 0xA9, 0x05, 0x61, 0x02, 0x00})
+
+		testA(t, cpu, 0x07)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x71ADCIndirectY(t *testing.T) {
+	t.Run("add with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0x32)
+		cpu.write(0x06, 0x12)
+		cpu.write(0x1234, 0x02)
+		cpu.LoadAndRun([]byte{0xA0, 0x02, 0xA9, 0x05, 0x71, 0x05, 0x00})
+
+		testA(t, cpu, 0x07)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x29ANDImmediate(t *testing.T) {
+	t.Run("and", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0b1111_0000, 0x29, 0b0011_1111, 0x00})
+
+		testA(t, cpu, 0b0011_0000)
+	})
+}
+
+func Test0x25ANDZeroPage(t *testing.T) {
+	t.Run("and", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0b0011_1111)
+		cpu.LoadAndRun([]byte{0xA9, 0b1111_0000, 0x25, 0x05, 0x00})
+
+		testA(t, cpu, 0b0011_0000)
+	})
+}
+
+func Test0x35ANDZeroPageX(t *testing.T) {
+	t.Run("and", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0b0011_1111)
+		cpu.LoadAndRun([]byte{0xA2, 0x01, 0xA9, 0b1111_0000, 0x35, 0x04, 0x00})
+
+		testA(t, cpu, 0b0011_0000)
+	})
+}
+
+func Test0x2DANDAbsolute(t *testing.T) {
+	t.Run("and", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0200, 0b0011_1111)
+		cpu.LoadAndRun([]byte{0xA9, 0b1111_0000, 0x2D, 0x00, 0x02, 0x00})
+
+		testA(t, cpu, 0b0011_0000)
+	})
+}
+
+func Test0x3DANDAbsoluteX(t *testing.T) {
+	t.Run("and", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0202, 0b0011_1111)
+		cpu.LoadAndRun([]byte{0xA2, 0x02, 0xA9, 0b1111_0000, 0x3D, 0x00, 0x02, 0x00})
+
+		testA(t, cpu, 0b0011_0000)
+	})
+}
+
+func Test0x39ANDAbsoluteY(t *testing.T) {
+	t.Run("and", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0202, 0b0011_1111)
+		cpu.LoadAndRun([]byte{0xA0, 0x02, 0xA9, 0b1111_0000, 0x39, 0x00, 0x02, 0x00})
+
+		testA(t, cpu, 0b0011_0000)
+	})
+}
+
+func Test0x21ANDIndirectX(t *testing.T) {
+	t.Run("and", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0x34)
+		cpu.write(0x06, 0x12)
+		cpu.write(0x1234, 0b0011_1111)
+
+		cpu.LoadAndRun([]byte{0xA2, 0x03, 0xA9, 0b1111_0000, 0x21, 0x02, 0x00})
+
+		testA(t, cpu, 0b0011_0000)
+	})
+}
+
+func Test0x31ANDIndirectY(t *testing.T) {
+	t.Run("and", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0x32)
+		cpu.write(0x06, 0x12)
+		cpu.write(0x1234, 0b0011_1111)
+		cpu.LoadAndRun([]byte{0xA0, 0x02, 0xA9, 0b1111_0000, 0x31, 0x05, 0x00})
+
+		testA(t, cpu, 0b0011_0000)
+	})
+}
+
+func Test0x0AASLAccumulator(t *testing.T) {
+	t.Run("shift left", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0b0000_0001, 0x0A, 0x00})
+
+		testA(t, cpu, 0b0000_0010)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+
+	t.Run("shift left with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.LoadAndRun([]byte{0xA9, 0b1000_0000, 0x0A, 0x00})
+
+		testA(t, cpu, 0b0000_0000)
+		testFlag(t, cpu, CARRY, true)
+		testFlag(t, cpu, ZERO, true)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x06ASLZeroPage(t *testing.T) {
+	t.Run("shift left", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0b0000_0001)
+		cpu.LoadAndRun([]byte{0x06, 0x05, 0x00})
+
+		testMem(t, cpu, 0x05, 0b0000_0010)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+
+	t.Run("shift left with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0b1000_0000)
+		cpu.LoadAndRun([]byte{0x06, 0x05, 0x00})
+
+		testMem(t, cpu, 0x05, 0b0000_0000)
+		testFlag(t, cpu, CARRY, true)
+		testFlag(t, cpu, ZERO, true)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x16ASLZeroPageX(t *testing.T) {
+	t.Run("shift left", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0b0000_0001)
+		cpu.LoadAndRun([]byte{0xA2, 0x01, 0x16, 0x04, 0x00})
+
+		testMem(t, cpu, 0x05, 0b0000_0010)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+
+	t.Run("shift left with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x05, 0b1000_0000)
+		cpu.LoadAndRun([]byte{0xA2, 0x01, 0x16, 0x04, 0x00})
+
+		testMem(t, cpu, 0x05, 0b0000_0000)
+		testFlag(t, cpu, CARRY, true)
+		testFlag(t, cpu, ZERO, true)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x0EASLAbsolute(t *testing.T) {
+	t.Run("shift left", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0200, 0b0000_0001)
+		cpu.LoadAndRun([]byte{0x0E, 0x00, 0x02, 0x00})
+
+		testMem(t, cpu, 0x0200, 0b0000_0010)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+
+	t.Run("shift left with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0200, 0b1000_0000)
+		cpu.LoadAndRun([]byte{0x0E, 0x00, 0x02, 0x00})
+
+		testMem(t, cpu, 0x0200, 0b0000_0000)
+		testFlag(t, cpu, CARRY, true)
+		testFlag(t, cpu, ZERO, true)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x1EASLAbsoluteX(t *testing.T) {
+	t.Run("shift left", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0202, 0b0000_0001)
+		cpu.LoadAndRun([]byte{0xA2, 0x01, 0x1E, 0x01, 0x02, 0x00})
+
+		testMem(t, cpu, 0x0202, 0b0000_0010)
+		testFlag(t, cpu, CARRY, false)
+		testFlag(t, cpu, ZERO, false)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+
+	t.Run("shift left with carry", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write16(0x0202, 0b1000_0000)
+		cpu.LoadAndRun([]byte{0xA2, 0x01, 0x1E, 0x01, 0x02, 0x00})
+
+		testMem(t, cpu, 0x0202, 0b0000_0000)
+		testFlag(t, cpu, CARRY, true)
+		testFlag(t, cpu, ZERO, true)
+		testFlag(t, cpu, NEGATIVE, false)
+	})
+}
+
+func Test0x4CJMPAbsolute(t *testing.T) {
+	t.Run("jump", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x0200, 0xA9)
+		cpu.write(0x0201, 0x05)
+		cpu.LoadAndRun([]byte{0x4C, 0x00, 0x02})
+
+		testA(t, cpu, 0x05)
+	})
+}
+
+func Test0x6CJMPIndirect(t *testing.T) {
+	t.Run("jump", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x0200, 0x67)
+		cpu.write(0x0201, 0x45)
+		cpu.write(0x4567, 0xA9)
+		cpu.write(0x4568, 0x55)
+		cpu.LoadAndRun([]byte{0x6C, 0x00, 0x02})
+
+		testA(t, cpu, 0x55)
+	})
+
+	t.Run("jump with page boundary", func(t *testing.T) {
+		cpu := new_cpu()
+		cpu.write(0x0200, 0x67)
+		cpu.write(0x02FF, 0x45)
+		cpu.write(0x0300, 0xA9)
+		cpu.write(0x6745, 0xA9)
+		cpu.write(0x6746, 0x55)
+		cpu.write(0xA945, 0xA9)
+		cpu.write(0xA946, 0x05)
+		cpu.LoadAndRun([]byte{0x6C, 0xFF, 0x02})
+
+		testA(t, cpu, 0x55)
+	})
+}
+
 // TEST HELPERS
 
 func testA(t *testing.T, cpu *Cpu, eq byte) {
@@ -505,5 +971,21 @@ func testFlag(t *testing.T, cpu *Cpu, flag StatusFlag, eq bool) {
 	t.Helper()
 	if cpu.getFlag(flag) != eq {
 		t.Errorf("Expected flag %v to be %v, got %v", flag, eq, cpu.getFlag(flag))
+	}
+}
+
+func testStackAddress(t *testing.T, cpu *Cpu, addr byte) {
+	t.Helper()
+	if cpu.SP != addr {
+		t.Errorf("Expected stack pointer to be 0x%02X, got 0x%02X", addr, cpu.SP)
+	}
+}
+
+func testStack(t *testing.T, cpu *Cpu, eq byte) {
+	t.Helper()
+	stackItem := cpu.read(STACK + uint16(cpu.SP) + 1)
+
+	if stackItem != eq {
+		t.Errorf("Expected stack item to be 0x%02X, got 0x%02X", eq, stackItem)
 	}
 }
